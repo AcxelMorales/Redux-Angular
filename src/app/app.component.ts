dbc9898db6bfc356011d59e59f42f8e215c845bc
Acxel Morales <acxelmorales97@gmail.com>
2023-07-25 09:44:15 -0600

    Counter example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { decrement, increment, reset } from './counter.actions';
+
+interface IStore {
+  count: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test';
+
+  count$: Observable<number> | undefined;
+
+  constructor(private readonly store: Store<IStore>) {
+    this.count$ = store.select('count');
+  }
+
+  public increment(): void {
+    this.store.dispatch(increment());
+  }
+
+  public decrement(): void {
+    this.store.dispatch(decrement());
+  }
+
+  public reset(): void {
+    this.store.dispatch(reset());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { counterReducer } from './counter.reducer';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      features: {
+        pause: false,
+        lock: true,
+        persist: true,
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
